f16b4422d1415edb42f0950b0e0663f9a7c8823a
Aric Gardner <actual@aricgardner.com>
2023-05-15 13:05:21 -0400

    [Main] Only detect 'txt' records once
    This change modifies the 'dns_monitor.py' script to enhance the detection of 'txt' records. The purpose is to ensure that a 'txt' record containing the specified substring ('google-site-verification') is only detected once. Subsequently, as the script iterates in the future, any 'txt' records that have already been seen will be ignored.
    
    The script now maintains a set of keys to keep track of the 'txt' records already encountered. During each iteration, it compares the new keys with the existing set to identify any new keys. If a new key is found, it prints a message indicating the discovery of the key and the relevant 'txt' record. If no new keys are found in a loop, it outputs a message indicating the absence of new keys.
    
    This change improves the efficiency and accuracy of the DNS monitoring process.

--- dns_monitor.py
+++ dns_monitor.py
@@ -6,19 +6,34 @@ import os
 
 # You can replace these with environment variables or command line arguments
 hostname = "futurestay.com"
-substring = "google-site"
+substring = "google-site-verification"
 d = 5
 
+# Store the keys
+keys = set()
+
 def monitor_dns(hostname, substring, d):
+    global keys
     while True:
         try:
+            new_keys = set()
+            found_new_key = False
             answers = dns.resolver.resolve(hostname, 'TXT')
             for rdata in answers:
                 for txt_string in rdata.strings:
-                    if substring in txt_string.decode():
-                        print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} - Found '{substring}' in TXT record of {hostname}")
+                    txt_string = txt_string.decode()
+                    if substring in txt_string:
+                        key = txt_string.split('=')[-1]
+                        new_keys.add(key)
+                        if key not in keys:
+                            found_new_key = True
+                            print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} - Found new '{substring}' key '{key}' in TXT record of {hostname}")
+            keys = new_keys
+            if not found_new_key:
+                print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} - No new keys found in this loop for {hostname}")
         except Exception as e:
             print(f"Error occurred: {e}")
         time.sleep(d)
 
 monitor_dns(hostname, substring, d)
+
